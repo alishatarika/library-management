2a7fe830b77da6710b364d3278c48928cba4c0b4
alishatarika <alisha.tarikaa@gmail.com>
2023-03-29 23:21:20 +0530

    addcprogram

--- library.c
+++ library.c
@@ -1,62 +1,81 @@
 #include <stdio.h>
 #include <string.h>
-struct student
+struct Student
 {
-    char name[23];
-    int rollnumber;
+    char name;
+    int rollNumber;
     char section;
 };
+void issuebook();
+void returnbook();
 int main()
 {
-    int n, date, month, year, date1, month1, year1;
-    char a[60];
-    char b[60];
-    struct student data;
+    int n;
+
+    struct Student data;
     printf("Enter student name ");
     scanf("%s", &data.name);
     printf("Enter student roll number ");
-    scanf("%d", &data.rollnumber);
+    scanf("%d", &data.rollNumber);
     printf("Enter section ");
     scanf("%s", &data.section);
 
     printf("1.Issue a book \n");
     printf("2.Want to return a book \n");
     printf("Enter a choice \n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     switch (n)
     {
     case 1:
-
-        printf("Which subject book do you want");
-        scanf("%s ", &a);
-        printf("Enter author name");
-        scanf("%s ", &b);
-        printf("issue date \n");
-        scanf("%d %d %d", &date, &month, &year);
-        break;
+        issuebook();
+         break;
     case 2:
-        printf("returning date \n");
-        scanf("%d %d %d", &date, &month, &year);
-
-        printf(" last date to returned book \n");
-        scanf("%d %d %d", &date1, &month1, &year1);
-        if (month == month1)
-        {
-            if (date >= date1)
-            {
-
-                printf("Book returned on time");
-            }
-        }
-        else
-        {
-            printf("fine will be imposed");
-            int a = date1 - date;
-
-            printf("%d", a * 2);
-        }
+        returnbook();
         break;
-    };
+    }
+}
+void issuebook()
+{
+    int date;
+    int month;
+    char a;
+    char b;
+    int year;
+    printf("Which subject book do you want ");
+    scanf("%s", &a);
+    printf("Enter author name ");
+    scanf("%s", &b);
+    printf("issue date \n");
+    scanf("%d %d %d", &date, &month, &year);
+}
 
-    return 0;
+void returnbook()
+{
+    int date;
+    int month;
+    int year;
+    int date1;
+    int month1;
+    int year1;
+
+    printf("returning date \n");
+    scanf("%d %d %d", &date, &month, &year);
+
+    printf(" last date to returned book \n");
+    scanf("%d %d %d", &date1, &month1, &year1);
+    if (month == month1)
+    {
+        if (date >= date1)
+        {
+
+            printf("Book returned on time");
+        }
+    }
+    else
+    {
+        printf("fine will be imposed");
+        int a = date1 - date;
+
+        printf("%d", a * 2);
+    }
 }
